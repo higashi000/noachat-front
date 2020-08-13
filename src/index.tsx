a45cd54b85b00161b4c2bd2a76219c47ed4d0d74
higashi000 <higashi.ict@gmail.com>
2020-08-13 17:55:15 +0900

    お引越し

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,62 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-const Hello: React.FunctionComponent<{ compiler: string, framework: string }> = (props) => {
-  return (
-    <div>
-      <div>{props.compiler}</div>
-      <div>{props.framework}</div>
-    </div>
-  );
+class Main extends React.Component {
+   constructor(props: any) {
+      super(props);
+
+      this.state = {
+         ws: null,
+         userName: '',
+         text: '',
+         messages: [],
+      };
+
+      this.ws: WebSocket;
+      this.handleTextChange = this.handleTextChange.bind(this);
+   }
+
+   handleTextChange(event: any) {
+      let name = event.target.name;
+
+      this.setState({[name]: event.target.value})
+   }
+
+   componentDidMount() {
+      let ws: WebSocket = new WebSocket('ws://localhost:5000');
+
+      ws.onopen = () => {
+         this.setState({ws: ws});
+      }
+
+      ws.onmessage = getMsg => {
+         console.log(getMsg.data);
+
+         let data: string = getMsg.data;
+
+         this.setState({
+            messages: [
+            ...this.state.messages,
+            {
+               getMsg,
+            }
+            ],
+            text: '',
+         })
+      }
+   }
+
+   sendMsg() {
+      this.state.ws.send('{"text":"' + this.state.userName + ": " + this.state.text + '"}')
+   }
+
+   render() {
+      return (
+         <div>
+            <TextField id="outlined-basic" name="userName" label="名前" variant="outlined" value={this.state.userName} onChange={this.handleTextChange} />
+         </div>
+      )
+   }
 }
-
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById("root")
-);
